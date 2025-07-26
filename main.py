1829a198884456ccaa7ec94d52bbbbb4a60b1e8a
JuPaRaMo <30596170+JuPaRaMo@users.noreply.github.com>
2025-07-26 16:20:16 -0500

    Update main.py nueva versoin

--- main.py
+++ main.py
@@ -4,49 +4,41 @@ from datetime import datetime
 from kaggle.api.kaggle_api_extended import KaggleApi
 from azure.storage.blob import BlobServiceClient
 
-# Configuration of ENV's
+# Configuración de variables de entorno
 DATASET = os.environ['KAGGLE_DATASET']
 DATE_COLUMN = os.environ['KAGGLE_DATE_COLUMN']
 CONTAINER_NAME = os.environ['AZURE_CONTAINER']
 AZURE_CONNECTION_STRING = os.environ['AZURE_STORAGE_CONNECTION_STRING']
+KAGGLE_USERNAME = os.environ['KAGGLE_USERNAME']
+KAGGLE_KEY = os.environ['KAGGLE_KEY']
 
-
-# Autentication on Kaggle dataset
-os.environ['KAGGLE_USERNAME'] = os.environ['KAGGLE_USERNAME']
-os.environ['KAGGLE_KEY'] = os.environ['KAGGLE_KEY']
+# Autenticación en Kaggle
+os.environ['KAGGLE_USERNAME'] = KAGGLE_USERNAME
+os.environ['KAGGLE_KEY'] = KAGGLE_KEY
 
 api = KaggleApi()
 api.authenticate()
 
-
-# Download dataset from Kaggle
+# Descargar dataset
 os.makedirs('data', exist_ok=True)
-api.dataset_download_files(DATASET, path='data_reemplazar', unzip=True)
+api.dataset_download_files(DATASET, path='data', unzip=True)
 
-
-# Leer CSV
+# Detectar archivo CSV
 csv_files = [f for f in os.listdir('data') if f.endswith('.csv')]
-print("Archivos encontrados en 'data':", csv_files)
-
 if not csv_files:
-    raise FileNotFoundError("No se encontró ningún archivo CSV en la carpeta 'data'. Verifica si el dataset se descargó correctamente.")
+    raise FileNotFoundError("No se encontró ningún archivo CSV en la carpeta 'data'.")
 
-csv_path = os.path.join('data', 'country_vaccinations.csv')
+csv_path = os.path.join('data', csv_files[0])  # Usa el primer CSV encontrado
 df = pd.read_csv(csv_path, parse_dates=[DATE_COLUMN])
 
-
-# Connection to Azure
+# Conexión a Azure
 blob_service_client = BlobServiceClient.from_connection_string(AZURE_CONNECTION_STRING)
 container_client = blob_service_client.get_container_client(CONTAINER_NAME)
 
-# partition by date
+# Particionar por fecha y subir
 for fecha, grupo in df.groupby(df[DATE_COLUMN].dt.date):
     nombre_archivo = f"proyecto_{fecha.strftime('%Y-%m-%d')}.csv"
     grupo.to_csv(nombre_archivo, index=False)
     with open(nombre_archivo, "rb") as data:
         blob_client = container_client.get_blob_client(nombre_archivo)
         blob_client.upload_blob(data, overwrite=True)
-
-
-
-
